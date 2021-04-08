dee6b9072278890d3bbd31aa1f4478fc4ea43c53
sarthak <sarthakagrawal8@gmail.com>
2021-04-08 20:46:02 +0530

    update makefile and checker

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 CC := gcc
 WFLAGS := -Wall -Wextra
 
-all: tests/gen ompver
+all: tests/gen ompver checker
 
 ompver: ompver.c
 	$(CC) -O0 -fopenmp $(WFLAGS) -o $@ $<
 
 tests/gen: tests/gen.c
-	gcc $(WFLAGS) -o $@ $<
+	$(CC) $(WFLAGS) -o $@ $<
+
+checker: checker.c
+	$(CC) -O3 -fopenmp $(WFLAGS) -o $@ $<
 
 N := 100
 seed := 0
@@ -17,18 +20,13 @@ strategy := 1
 num_threads := 4
 
 clean:
-	rm -rf testmatrix temp/
-	rm -f output_*.txt
+	@rm -f testmatrix
+	@rm -f output_*.txt
 
-check: clean ompver tests/gen
-	./tests/gen $(N) $(seed) $(s) $(avg) > testmatrix
-	./ompver $(N) $(N) testmatrix $(num_threads) 1
-	@mkdir temp/
-	@mv output_L_1_$(num_threads).txt \
-	   output_D_1_$(num_threads).txt \
-	   output_U_1_$(num_threads).txt \
-	   temp/
-	time ./ompver $(N) $(N) testmatrix $(num_threads) $(strategy)
-	@diff -s temp/output_L_1_$(num_threads).txt output_L_$(strategy)_$(num_threads).txt
-	@diff -s temp/output_D_1_$(num_threads).txt output_D_$(strategy)_$(num_threads).txt
-	@diff -s temp/output_U_1_$(num_threads).txt output_U_$(strategy)_$(num_threads).txt
+check: clean ompver tests/gen checker
+	@./tests/gen $(N) $(seed) $(s) $(avg) > testmatrix
+	@time ./ompver $(N) $(N) testmatrix $(num_threads) $(strategy)
+	@./checker $(N) $(N) \
+	  testmatrix output_L_$(strategy)_$(num_threads).txt \
+	  output_D_$(strategy)_$(num_threads).txt \
+	  output_U_$(strategy)_$(num_threads).txt
---
+++ checker.c
@@ -0,0 +1,82 @@
+#include <assert.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define EPSILON 1e-3
+
+double dabs(double a) { return a < 0 ? -a : a; }
+
+double **alloc_matrix(int n, int m) {
+    double **mat = (double **)malloc(sizeof(double *) * n);
+#pragma omp parallel for shared(mat, n, m)
+    for (int i = 0; i < n; i++) {
+        mat[i] = (double *)malloc(sizeof(double) * m);
+        double *row = mat[i];
+        for (int j = 0; j < m; j++)
+            row[j] = 0;
+    }
+    return mat;
+}
+
+void read_matrix(FILE *input, double **mat, int n, int m) {
+    double num;
+    for (int i = 0; i < n; i++) {
+        double *row = mat[i];
+        for (int j = 0; j < m; j++) {
+            fscanf(input, "%lf ", &num);
+            row[j] = num;
+        }
+    }
+}
+
+void multiply_matrix(double** A, double** B, double** C, int n) {
+#pragma omp parallel for shared(A, B, C, n)
+  for (int i=0; i<n; i++) {
+    double* row = C[i];
+    for (int j=0; j<n; j++) {
+      double res = 0;
+      for (int k=0; k<n; k++) {
+        res += A[i][k] * B[k][j];
+      }
+      row[j] = res;
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  assert (argc == 7);
+  int n = atoi(argv[1]), m = atoi(argv[2]);
+
+  double **A = alloc_matrix(n, m),
+         **L = alloc_matrix(n, m),
+         **D = alloc_matrix(n, m),
+         **U = alloc_matrix(n, m),
+         **X = alloc_matrix(n, m),
+         **Y = alloc_matrix(n, m);
+
+  //read matrices
+  FILE *afile = fopen(argv[3], "r"), *lfile = fopen(argv[4], "r"),
+       *dfile = fopen(argv[5], "r"), *ufile = fopen(argv[6], "r");
+  read_matrix(afile, A, n, m);
+  read_matrix(lfile, L, n, m);
+  read_matrix(dfile, D, n, m);
+  read_matrix(ufile, U, n, m);
+
+  //multiply matrices
+  multiply_matrix(L, D, X, n);
+  multiply_matrix(X, U, Y, n);
+
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<m; j++) {
+        if (dabs(A[i][j] - Y[i][j]) > EPSILON) {
+            fprintf(stderr,
+                    "A != LDU (match fail at row=%d col=%d : %lf != %lf) \n", i,
+                    j, A[i][j], Y[i][j]);
+            return 0;
+        }
+    }
+  }
+
+  printf("Verified A = L D U : pass\n");
+}
