70e959f09acf60b5bc57152c0f1b553d021962c8
Navneel Singhal <navneel.singhal@ymail.com>
2021-04-10 10:29:02 +0530

    default to Allgather implementation

--- mpiver.c
+++ mpiver.c
@@ -326,6 +326,87 @@ void crout_transpose_contiguous_buffer(double **A, double **L, double **U,
     transpose_matrix(U, n);
 }
 
+void crout_gatherall(double **A, double **L, double **U, int n) {
+
+    int i, j, k;
+    double sum = 0;
+
+    /* Let each worker do this part O(n) */
+    for (i = 0; i < n; i++) {
+        U[i][i] = 1;
+    }
+
+    /* Allocate a buffer of size 2*n for communication */
+    /* Stack should be sufficient (16kB for n~1024) */
+    double buffer[2 * chunk_size(0, n) * num_processes];
+    double recv_buffer[2 * chunk_size(0, n) * num_processes];
+
+    for (j = 0; j < n; j++) {
+
+        double *Lj = L[j];
+        double *Uj = U[j];
+
+        /* Let each worker compute L[j][j] on its own O(n) */
+        sum = 0;
+        for (k = 0; k < j; k++)
+            sum += Lj[k] * Uj[k];
+        Lj[j] = A[j][j] - sum;
+        if (Lj[j] == 0) {
+            fprintf(stderr, "Fatal: Non-decomposable\n");
+            MPI_Abort(MPI_COMM_WORLD, -1);
+        }
+
+        /* We now need to iterate from j+1 -> n in num_processes chunks.
+         * Verify that :
+         *      st[rank=0] = 0
+         *      en[rank=n-1] = n
+         *      en[rank=i] = st[rank=i+1]
+         */
+
+        if (j == n - 1) {
+            // we probably don't need to compute the rest of it
+            break;
+        }
+
+        // chunk size becomes 0 if j == n - 1,
+        // since there are really no iterations to be done
+        int size = chunk_size(j + 1, n);
+        int st = j + 1 + rank * size;
+        int en = min(n, st + size);
+
+        for (i = st; i < en; i++) {
+
+            double *Li = L[i];
+            double *Ui = U[i];
+
+            sum = 0;
+            for (k = 0; k < j; k++)
+                sum += Li[k] * Uj[k];
+            /* L[i][j] = A[i][j] - sum; */
+            buffer[i - st] = A[i][j] - sum;
+
+            sum = 0;
+            for (k = 0; k < j; k++)
+                sum += Lj[k] * Ui[k];
+            /* U[j][i] = (A[j][i] - sum) / L[j][j]; */
+            buffer[i - st + size] = (A[j][i] - sum) / L[j][j];
+        }
+
+        /* Gather all results of this iteration into master */
+        MPI_Allgather(buffer, 2 * size, MPI_DOUBLE, recv_buffer, 2 * size, MPI_DOUBLE, MPI_COMM_WORLD);
+
+        /* Copy buffer into respective matrices */
+        for (i = j + 1; i < n; i++) {
+            L[i][j] = recv_buffer[i - (j + 1)];
+            U[i][j] = recv_buffer[i - (j + 1) + size];
+        }
+
+        /* Brace for next iteration */
+    }
+
+    transpose_matrix(U, n);
+}
+
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -376,7 +457,8 @@ int main(int argc, char **argv) {
     TIMEIT_START;
     // crout(A, L, U, n);
     // crout_transpose(A, L, U, n);
-    crout_transpose_contiguous_buffer(A, L, U, n);
+    // crout_transpose_contiguous_buffer(A, L, U, n);
+    crout_gatherall(A, L, U, n);
     TIMEIT_END("Decomposition");
 
     /* All processes other than master can exit */
