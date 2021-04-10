e5b558a3a5ecfaefedcd29114dbaf8c4c9bc9e4f
sarthak <sarthakagrawal8@gmail.com>
2021-04-10 11:37:22 +0530

    crout async

--- mpiver.c
+++ mpiver.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <mpi.h>
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -407,6 +408,112 @@ void crout_gatherall(double **A, double **L, double **U, int n) {
     transpose_matrix(U, n);
 }
 
+void crout_async(double **A, double **L, double **U, int n) {
+
+    int i, j, k;
+    double sum = 0;
+
+    /* Let each worker do this part O(n) */
+    for (i = 0; i < n; i++) {
+        U[i][i] = 1;
+    }
+
+    /* Allocate a buffers of size 2*n for communication */
+    /* Stack should be sufficient (64kB for n~1024) */
+    double send_buffer[2][2 * chunk_size(0, n) * num_processes];
+    double recv_buffer[2][2 * chunk_size(0, n) * num_processes];
+    int turn = 0; // Which buffer to use this iteration
+    MPI_Request allgather_request = MPI_REQUEST_NULL;
+
+    for (j = 0; j < n; j++) {
+
+        double *Lj = L[j];
+        double *Uj = U[j];
+
+        /* Let each worker compute L[j][j] on its own O(n) */
+        sum = 0;
+        for (k = 0; k < j - 1; k++)
+            sum += Lj[k] * Uj[k];
+        Lj[j] = A[j][j] - sum;
+
+        int size = chunk_size(j + 1, n);
+        int st = j + 1 + rank * size;
+        int en = min(n, st + size);
+
+        for (i = st; i < en; i++) {
+            double *Li = L[i];
+            double *Ui = U[i];
+
+            sum = 0;
+            for (k = 0; k < j - 1; k++)
+                sum += Li[k] * Uj[k];
+            /* L[i][j] = A[i][j] - sum; */
+            send_buffer[turn][i - st] = A[i][j] - sum;
+
+            sum = 0;
+            for (k = 0; k < j - 1; k++)
+                sum += Lj[k] * Ui[k];
+            /* U[j][i] = (A[j][i] - sum) / L[j][j]; */
+            send_buffer[turn][i - st + size] = (A[j][i] - sum);
+        }
+
+        if (allgather_request != MPI_REQUEST_NULL) {
+            int flag = 0;
+            while (!flag) {
+                MPI_Test(&allgather_request, &flag, MPI_STATUS_IGNORE);
+                sched_yield();
+            }
+            /* Copy buffer into respective matrices */
+            for (i = j; i < n; i++) {
+                L[i][j - 1] = recv_buffer[turn][i - j];
+                U[i][j - 1] = recv_buffer[turn][i - j + size];
+            }
+        }
+
+        if (j == n - 1) {
+            // we probably don't need to compute the rest of it
+            break;
+        }
+
+        sum = 0;
+        for (k = j - 1; k < j; k++)
+            sum += Lj[k] * Uj[k];
+        Lj[j] -= sum;
+        if (Lj[j] == 0) {
+            fprintf(stderr, "Fatal: Non-decomposable\n");
+            MPI_Abort(MPI_COMM_WORLD, -1);
+        }
+
+        for (i = st; i < en; i++) {
+            double *Li = L[i];
+            double *Ui = U[i];
+
+            sum = 0;
+            for (k = j - 1; k < j; k++)
+                sum += Li[k] * Uj[k];
+            /* L[i][j] = A[i][j] - sum; */
+            send_buffer[turn][i - st] -= sum;
+
+            sum = 0;
+            for (k = 0; k < j; k++)
+                sum += Lj[k] * Ui[k];
+            /* U[j][i] = (A[j][i] - sum) / L[j][j]; */
+            send_buffer[turn][i - st + size] =
+                (send_buffer[turn][i - st + size] - sum) / L[j][j];
+        }
+
+        /* Gather all results of this iteration into master */
+        MPI_Iallgather(send_buffer[turn], 2 * size, MPI_DOUBLE,
+                       recv_buffer[(turn + 1) % 2], 2 * size, MPI_DOUBLE,
+                       MPI_COMM_WORLD, &allgather_request);
+
+        /* Brace for next iteration */
+        turn = (turn + 1) % 2;
+    }
+
+    transpose_matrix(U, n);
+}
+
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -458,7 +565,8 @@ int main(int argc, char **argv) {
     // crout(A, L, U, n);
     // crout_transpose(A, L, U, n);
     // crout_transpose_contiguous_buffer(A, L, U, n);
-    crout_gatherall(A, L, U, n);
+    // crout_gatherall(A, L, U, n);
+    crout_async(A, L, U, n);
     TIMEIT_END("Decomposition");
 
     /* All processes other than master can exit */
