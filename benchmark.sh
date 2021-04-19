8181a8f2cdb1d9193dc985a0833051b4c89a223c
Navneel Singhal <navneel.singhal@ymail.com>
2021-04-19 16:38:05 +0530

    add mpi benchmarks

--- benchmark.sh
+++ benchmark.sh
@@ -1,18 +1,18 @@
-for n in 5000
+for n in 200 1000 5000
 do
     make gen N=$n
     for threads in 1 2 4 8 16
     do
         if [[ threads -ge 4 ]]
         then
-            for strategy in 1 2 3
+            for strategy in 4
             do
                 echo N = $n, Threads = $threads, Strategy = $strategy
                 time bash run.sh $n testmatrix $threads $strategy
                 echo
             done
         else
-            for strategy in 0 1 2 3
+            for strategy in 4
             do
                 echo N = $n, Threads = $threads, Strategy = $strategy
                 time bash run.sh $n testmatrix $threads $strategy
